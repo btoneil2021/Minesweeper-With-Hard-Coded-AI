9edb58693e342cfaa4cbca44adf6ef034af75119
btoneil2021 <boneil3@villanova.edu>
2025-10-10 13:56:20 -0400

    Refactoring constraint_subtractor class

--- Minesweeper/ai/constraint_subtractor.py
+++ Minesweeper/ai/constraint_subtractor.py
@@ -1,5 +1,9 @@
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Set, NamedTuple
 from constants import *
+
+class TileConstraint(NamedTuple):
+    unknowns: Set[Tuple[int, int]]
+    mines_needed: int
 
 class ConstraintSubtractor:
     def __init__(self, board_analyzer):
@@ -26,61 +30,57 @@ class ConstraintSubtractor:
               or tile_value == 0:
             return None
         
-        unknowns_a, mines_needed_a = self._get_tile_context(tile_coord, tile_value)
+        constraint_a = self._get_tile_context(tile_coord, tile_value)
 
-        if not unknowns_a or mines_needed_a <= 0:
+        if not constraint_a.unknowns or constraint_a.mines_needed <= 0:
             return None
 
-        return self._check_and_return_two_hop_neighbors(tile_coord, unknowns_a, mines_needed_a)
+        return self._check_and_return_two_hop_neighbors(tile_coord, constraint_a)
     
-    def _get_tile_context(self, tile_coord, tile_value):
+    def _get_tile_context(self, tile_coord, tile_value) -> TileConstraint:
         flags_around = self.analyzer.count_neighbors_by_states(tile_coord, [AI_FLAGGED])
-
-        return (
-            set(self.analyzer.get_neighbors_by_state(tile_coord, AI_UNKNOWN)),
-            tile_value - flags_around
-        )
+        unknowns = set(self.analyzer.get_neighbors_by_state(tile_coord, AI_UNKNOWN))
+        mines_needed = tile_value - flags_around
+        
+        return TileConstraint(unknowns, mines_needed)
     
-    def _check_and_return_two_hop_neighbors(self, tile_coord, unknowns_a, mines_needed_a):
+    def _check_and_return_two_hop_neighbors(self, tile_coord, constraint_a: TileConstraint):
         for neighbor_coord in self.analyzer.get_two_hop_neighbors(tile_coord):
             if (neighbor_value := self.analyzer.get_tile_value_if_valid(neighbor_coord)) is None \
                   or neighbor_value == 0 \
                     or neighbor_value in [AI_FLAGGED, AI_UNKNOWN]:
                 continue
 
-            unknowns_b, mines_needed_b = self._get_tile_context(neighbor_coord, neighbor_value)
+            constraint_b = self._get_tile_context(neighbor_coord, neighbor_value)
 
-            if not unknowns_b or mines_needed_b <= 0:
+            if not constraint_b.unknowns or constraint_b.mines_needed <= 0:
                 continue
 
-            result_tile_coords = self._unknowns_subset_of_other_unknowns(unknowns_a, mines_needed_a,
-                                                                       unknowns_b, mines_needed_b)
+            result_tile_coords = self._unknowns_subset_of_other_unknowns(constraint_a, constraint_b)
             if result_tile_coords is not None:
                 return result_tile_coords
 
-            result_tile_coords = self._unknowns_subset_of_other_unknowns(unknowns_b, mines_needed_b,
-                                                                       unknowns_a, mines_needed_a)
+            result_tile_coords = self._unknowns_subset_of_other_unknowns(constraint_b, constraint_a)
             if result_tile_coords is not None:
                 return result_tile_coords
 
-            result_tile_coords = self._overlapping_constraints_case(unknowns_a, mines_needed_a,
-                                                                    unknowns_b, mines_needed_b)
+            result_tile_coords = self._overlapping_constraints_case(constraint_a, constraint_b)
             if result_tile_coords is not None:
                 return result_tile_coords
-            
+
         return None
     
-    def _unknowns_subset_of_other_unknowns(self, unknowns_a, mines_needed_a, unknowns_b, mines_needed_b):
-        if not unknowns_b.issubset(unknowns_a):
+    def _unknowns_subset_of_other_unknowns(self, constraint_a: TileConstraint, constraint_b: TileConstraint):
+        if not constraint_b.unknowns.issubset(constraint_a.unknowns):
             return None
-        
-        difference = unknowns_a - unknowns_b
+
+        difference = constraint_a.unknowns - constraint_b.unknowns
 
         # Case 1: All difference tiles must be mines
         # A needs N mines in {B's tiles + difference}
         # B needs M mines in {B's tiles}
         # If N - M = len(difference), all difference tiles are mines
-        if difference and mines_needed_a - mines_needed_b == len(difference):
+        if difference and constraint_a.mines_needed - constraint_b.mines_needed == len(difference):
             # Flag one of the difference tiles
             tile_to_flag = next(iter(difference))
             return (-tile_to_flag[0], -tile_to_flag[1])
@@ -88,45 +88,43 @@ class ConstraintSubtractor:
         # Case 2: All difference tiles must be safe
         # If N = M, then B accounts for all of A's mines
         # So difference tiles are safe
-        if difference and mines_needed_a == mines_needed_b:
+        if difference and constraint_a.mines_needed == constraint_b.mines_needed:
             # Reveal one of the difference tiles
             return next(iter(difference))
     
-    def _overlapping_constraints_case(self, unknowns_a, mines_needed_a, unknowns_b, mines_needed_b):
+    def _overlapping_constraints_case(self, constraint_a: TileConstraint, constraint_b: TileConstraint):
         # Case 3: Overlapping constraints (neither is subset of the other)
         # Example: A sees {X, Y, Z} needs 2, B sees {Y, Z, W} needs 1
         # Overlap {Y, Z} can have at most 1 mine, so X must be a mine
-        intersection = unknowns_a & unknowns_b
-
-        if not intersection or intersection == unknowns_a or intersection == unknowns_b:
+        if not self._is_single_tile_in_difference(constraint_a.unknowns, constraint_b.unknowns):
             return None
+
+        intersection = constraint_a.unknowns & constraint_b.unknowns
+        diff_a = constraint_a.unknowns - constraint_b.unknowns
+        diff_b = constraint_b.unknowns - constraint_a.unknowns
+        tile_a, tile_b = next(iter(diff_a)), next(iter(diff_b))
+
+        max_intersection_mines = min(len(intersection), constraint_a.mines_needed, constraint_b.mines_needed)
+        min_intersection_mines = max(0, constraint_a.mines_needed - 1, constraint_b.mines_needed - 1)
+
+        if constraint_a.mines_needed > max_intersection_mines: return (-tile_a[0], -tile_a[1])
+        if constraint_b.mines_needed > max_intersection_mines: return (-tile_b[0], -tile_b[1])
+        if constraint_a.mines_needed == min_intersection_mines: return tile_a
+        if constraint_b.mines_needed == min_intersection_mines: return tile_b
+
+        return None
+    
+    def _is_single_tile_in_difference(self, unknowns_a, unknowns_b):
+        if not self._is_intersection(unknowns_a, unknowns_b):
+            return False
         
         diff_a = unknowns_a - unknowns_b
         diff_b = unknowns_b - unknowns_a
-
-        if len(diff_a) != 1 or len(diff_b) != 1:
-            return None
         
-        # Single tile in each difference - can deduce exact values
-        tile_a = next(iter(diff_a))
-        tile_b = next(iter(diff_b))
-
-        max_intersection_mines = min(len(intersection), mines_needed_a, mines_needed_b)
-
-        min_intersection_mines = max(0,
-                                    mines_needed_a - len(diff_a),
-                                    mines_needed_b - len(diff_b))
-
-        if mines_needed_a > max_intersection_mines:
-            return (-tile_a[0], -tile_a[1])
-
-        if mines_needed_b > max_intersection_mines:
-            return (-tile_b[0], -tile_b[1])
-
-        if mines_needed_a == min_intersection_mines:
-            return tile_a
-
-        if mines_needed_b == min_intersection_mines:
-            return tile_b
+        return (len(diff_a) == 1 and len(diff_b) == 1)
         
-        return None
+    def _is_intersection(self, unknowns_a, unknowns_b):
+        intersection = unknowns_a & unknowns_b
+
+        return (intersection and intersection != unknowns_a and intersection != unknowns_b)
+    
